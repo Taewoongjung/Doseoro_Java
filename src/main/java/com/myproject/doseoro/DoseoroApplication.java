43129cb204079d943c384431723b1b0bfbd4afe0
Taewoongjung <aipooh8882@naver.com>
2022-08-06 00:55:11 +0900

    first commit

---
+++ src/main/java/com/myproject/doseoro/DoseoroApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.doseoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoseoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoseoroApplication.class, args);
+	}
+
+}
