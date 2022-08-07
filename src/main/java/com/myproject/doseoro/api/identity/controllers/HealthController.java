72416ee59a91267e5ffc28cf3317bedb3dc45b71
Taewoongjung <aipooh8882@naver.com>
2022-08-07 23:13:39 +0900

    Revise app.properties

--- src/main/java/com/myproject/doseoro/api/identity/controllers/HealthController.java
+++ src/main/java/com/myproject/doseoro/api/identity/controllers/HealthController.java
@@ -3,16 +3,19 @@ package com.myproject.doseoro.api.identity.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class HealthController {
 
     @RequestMapping(value = "/")
-    public String home() {
+    public String home(Map<String, Object> model) {
+        model.put("message", "HowToDoInJava Reader !!");
         return "home";
     }
 
     @RequestMapping(value = "/idx")
-    public String index() {
+    public String index(Map<String, Object> model) {
         return "index";
     }
 }
