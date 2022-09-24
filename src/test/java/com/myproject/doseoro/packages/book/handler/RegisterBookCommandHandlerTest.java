b9cbf288dceb8b56f53b1fadb8880b5690c7a881
Taewoongjung <aipooh8882@naver.com>
2022-09-24 17:01:53 +0900

    [리팩토링] 프로퍼티 파일 테스트용 분리

--- src/test/java/com/myproject/doseoro/packages/book/handler/RegisterBookCommandHandlerTest.java
+++ src/test/java/com/myproject/doseoro/packages/book/handler/RegisterBookCommandHandlerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
