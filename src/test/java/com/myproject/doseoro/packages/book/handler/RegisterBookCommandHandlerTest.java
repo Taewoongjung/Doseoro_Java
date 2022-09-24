f1b9f54ff7f014ef6af1c2bdfa53d0ad69df7ab3
Taewoongjung <aipooh8882@naver.com>
2022-09-24 21:52:10 +0900

    Final try CI

--- src/test/java/com/myproject/doseoro/packages/book/handler/RegisterBookCommandHandlerTest.java
+++ src/test/java/com/myproject/doseoro/packages/book/handler/RegisterBookCommandHandlerTest.java
@@ -12,9 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
