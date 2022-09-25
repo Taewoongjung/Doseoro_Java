c8a581b2988c797bb79abf9e6a4e8610619ecfad
Taewoongjung <aipooh8882@naver.com>
2022-09-26 03:01:11 +0900

    [추가] t_book_like 테이블 추가

---
+++ src/main/java/com/myproject/doseoro/database/book_like.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `t_book_like`
+(
+    `seq`           BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id`            VARCHAR(64)  NOT NULL  COMMENT '좋아요 정보의 id',
+    `identity_id`   VARCHAR(64)  NOT NULL  COMMENT 'book_id로 id를 가진 책에 좋아요 누른 사람 id',
+    `book_id`       VARCHAR(64)  NOT NULL  COMMENT 'identity_id로 id를 가진 사람이 누른 책의 id',
+    `createdAt`     TIMESTAMP    NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt`     TIMESTAMP    NOT NULL  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`seq`),
+    UNIQUE KEY `uix-book-id` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT '책을 좋아요 누른 사람의 정보';
--- src/main/java/com/myproject/doseoro/packages/book/domain/Book.java
+++ src/main/java/com/myproject/doseoro/packages/book/domain/Book.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 @ToString
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
