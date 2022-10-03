6173eea4bad2530ede0724ef009115a60356ef7d
Taewoongjung <aipooh8882@naver.com>
2022-10-03 22:43:51 +0900

    로그 기능 발전시키기

--- src/main/java/com/myproject/doseoro/adaptor/logger/Logging.java
+++ src/main/java/com/myproject/doseoro/adaptor/logger/Logging.java
@@ -7,4 +7,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Logging {}
+public @interface Logging {
+    String level() default "info";
+}
