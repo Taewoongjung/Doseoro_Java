467c8eda74fc2d541d125f9b4e96cf7fff9c0f6c
Taewoongjung <aipooh8882@naver.com>
2022-10-03 19:56:56 +0900

    GetUserInformationQuery 테스트 코드 리팩터링

--- src/test/java/com/myproject/doseoro/application/identity/readmodel/GetUserInformationQueryTest.java
+++ src/test/java/com/myproject/doseoro/application/identity/readmodel/GetUserInformationQueryTest.java
@@ -1,18 +1,68 @@
 package com.myproject.doseoro.application.identity.readmodel;
 
+import com.myproject.doseoro.adaptor.global.util.session.AccessUserSessionManager;
+import com.myproject.doseoro.application.abstraction.IdentityRepository;
+import com.myproject.doseoro.application.identity.dto.GetUserInformationDtoResult;
+import com.myproject.doseoro.application.identity.vo.IdentityMyPageVO;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class GetUserInformationQueryTest {
 
-    GetUserInformationQuery getUserInformationQuery = mock(GetUserInformationQuery.class);
+    @InjectMocks
+    GetUserInformationQuery getUserInformationQuery;
+
+    @Mock
+    private IdentityRepository identityRepository;
+
+    @Mock
+    private AccessUserSessionManager accessUserSessionManager;
 
     @Test
     @DisplayName("해당 유저의 유저 정보를 불러온다.")
     void getUser() {
-        getUserInformationQuery.query(null);
-        verify(getUserInformationQuery, times(1)).query(null);
+
+        IdentityMyPageVO identity = new IdentityMyPageVO(
+                "12312315256787",
+                "a@a.com",
+                "홍길동",
+                "길도이",
+                "010-2111-7777",
+                "경기도 용인시 보정동",
+                "보정동",
+                "용인시",
+                "경기도",
+                "kakao",
+                "12321-11",
+                "11-11-11"
+        );
+
+        when(accessUserSessionManager.extractUser()).thenReturn("12312315256787");
+        when(identityRepository.findUserById("12312315256787")).thenReturn(identity);
+
+        GetUserInformationDtoResult actual = getUserInformationQuery.query(null);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getUser().getId()).isEqualTo("12312315256787");
+        assertThat(actual.getUser().getEmail()).isEqualTo("a@a.com");
+        assertThat(actual.getUser().getName()).isEqualTo("홍길동");
+        assertThat(actual.getUser().getNickName()).isEqualTo("길도이");
+        assertThat(actual.getUser().getPhone()).isEqualTo("010-2111-7777");
+        assertThat(actual.getUser().getLocation()).isEqualTo("경기도 용인시 보정동");
+        assertThat(actual.getUser().getDong()).isEqualTo("보정동");
+        assertThat(actual.getUser().getSi()).isEqualTo("용인시");
+        assertThat(actual.getUser().getDou()).isEqualTo("경기도");
+        assertThat(actual.getUser().getProvider()).isEqualTo("kakao");
+        assertThat(actual.getUser().getSnsId()).isEqualTo("12321-11");
+        assertThat(actual.getUser().getUpdatedAt()).isEqualTo("11-11-11");
     }
 }
