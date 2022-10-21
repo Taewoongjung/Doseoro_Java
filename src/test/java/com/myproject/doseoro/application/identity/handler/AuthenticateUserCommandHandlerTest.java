676c43e369606bb186aaef48d0c3682d786910de
Taewoongjung <aipooh8882@naver.com>
2022-10-21 23:50:59 +0900

    CI 에서만 테스트 통과 안하는 버그 수정

--- src/test/java/com/myproject/doseoro/application/identity/handler/AuthenticateUserCommandHandlerTest.java
+++ src/test/java/com/myproject/doseoro/application/identity/handler/AuthenticateUserCommandHandlerTest.java
@@ -6,7 +6,6 @@ import com.myproject.doseoro.adaptor.infra.dao.IdentityDao;
 import com.myproject.doseoro.adaptor.infra.mybatis.identity.IdentityMybatisRepository;
 import com.myproject.doseoro.application.identity.vo.IdentityVO;
 import com.myproject.doseoro.application.identity.vo.SignUpVO;
-import com.myproject.doseoro.identity.IdentityVOFixture;
 import com.myproject.doseoro.identity.SignUpVOFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.myproject.doseoro.identity.IdentityVOFixture.identityVO;
+import static com.myproject.doseoro.identity.SignUpVOFixture.signUpVO;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -38,26 +39,26 @@ class AuthenticateUserCommandHandlerTest {
         AuthenticateUserCommandHandler sut = new AuthenticateUserCommandHandler(repository, accessUserSessionManager);
 
         SignUpVO signUpUser = new SignUpVO(
-                "7777777",
-                "abcdefg@naver.com",
-                "aa",
-                "홍길동",
-                "길동이",
-                "010-1234-5678",
-                "좋아하는 추억",
-                "많은 추억"
+                signUpVO.getId(),
+                signUpVO.getEmail(),
+                signUpVO.getPassword(),
+                signUpVO.getName(),
+                signUpVO.getNickName(),
+                signUpVO.getPhoneNumber(),
+                signUpVO.getQuestion(),
+                signUpVO.getAnswerForQuestion()
         );
         createUserIdentityCommandHandler.handle(signUpUser);
 
         IdentityVO loginUser = new IdentityVO(
-                "7777777",
-                "abcdefg@naver.com",
-                "aa",
-                "홍길동",
-                "길동이",
-                "010-1234-5678",
-                "좋아하는 추억",
-                "많은 추억"
+                identityVO.getId(),
+                identityVO.getEmail(),
+                identityVO.getPassword(),
+                identityVO.getName(),
+                identityVO.getNickName(),
+                identityVO.getPhone(),
+                identityVO.getForgotPwQuestion(),
+                identityVO.getForgotPwAnswer()
         );
 
         // when
@@ -79,7 +80,7 @@ class AuthenticateUserCommandHandlerTest {
         SignUpVO signUpUser = SignUpVOFixture.signUpVOWhenMakesWrong;
         createUserIdentityCommandHandler.handle(signUpUser);
 
-        IdentityVO loginUser = IdentityVOFixture.identityVO;
+        IdentityVO loginUser = identityVO;
 
         // when
         // then
