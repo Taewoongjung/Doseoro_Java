c6b4efff18bb1a24280ee966920321bea81977dd
Taewoongjung <aipooh8882@naver.com>
2022-11-16 00:29:27 +0900

    makeListOfBookId 메소드 따로 빼기

--- src/main/java/com/myproject/doseoro/application/book/readmodel/GetLikedBooksByUserQuery.java
+++ src/main/java/com/myproject/doseoro/application/book/readmodel/GetLikedBooksByUserQuery.java
@@ -26,9 +26,8 @@ public class GetLikedBooksByUserQuery implements CommandQuery<Void, GetLikedBook
             return null;
         }
 
-        List<AllLikedBookVO> foundAllBooks = bookMybatisRepository.allLikedBook(userId);
-        List<String> listOfBookId = foundAllBooks.stream()
-            .map(AllLikedBookVO::getBookId).toList();
+        List<AllLikedBookVO> foundAllBooks = getAllLikedBook(userId);
+        List<String> listOfBookId = makeListOfBookId(foundAllBooks);
 
         List<BookVO> books = new ArrayList<>();
         for (int i = 0; i < listOfBookId.size(); i++) {
@@ -40,5 +39,20 @@ public class GetLikedBooksByUserQuery implements CommandQuery<Void, GetLikedBook
 
     private String getUserIdFromSession() {
         return accessUserSessionManager.extractUser();
+    }
+
+    private List<AllLikedBookVO> getAllLikedBook(String userId) {
+        return bookMybatisRepository.allLikedBook(userId);
+    }
+
+    private List<String> makeListOfBookId(List<AllLikedBookVO> list) {
+        return list.stream().map(AllLikedBookVO::getBookId).toList();
+    }
+
+    private List<BookVO> makeListOfBooks(List<String> listOfBookId) {
+        List<BookVO> books = new ArrayList<>();
+        for (String bookId : listOfBookId) {
+            books.add(bookMybatisRepository.findBookByBookId(listOfBookId.get(bookId)));
+        }
     }
 }
