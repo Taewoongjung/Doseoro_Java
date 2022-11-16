327bddf46f15c5e709e7c7a6e627d77e378e7529
Taewoongjung <aipooh8882@naver.com>
2022-11-16 20:59:37 +0900

    GetAllDonationBooksQuery 테스트 코드 추가

--- src/testFixtures/java/com/myproject/doseoro/book/FindAllBooksVOFixture.java
+++ src/testFixtures/java/com/myproject/doseoro/book/FindAllBooksVOFixture.java
@@ -86,4 +86,56 @@ public class FindAllBooksVOFixture {
             .ownerId("111")
             .build();
     }
+
+    public static final FindAllBooksVO firstDonationBook() {
+        List<String> checkCategory = new ArrayList<>();
+        checkCategory.add("SF");
+        checkCategory.add("만화");
+
+        List<String> checkState = new ArrayList<>();
+        checkState.add("깨끗");
+        checkState.add("거의 새책");
+
+        List<String> images = new ArrayList<>();
+        images.add("imgs1e51ca13-e32e-4db8-a308-bf4ccdd2c15f");
+        images.add("imgs0c1d16f2-a946-468f-8bcc-ffdc8f57b2fe");
+
+        return FindAllBooksVO.builder()
+            .id("123")
+            .postMessage("책 팔아요 ~")
+            .title("읽으면 좋은 책")
+            .author("천재")
+            .publisher("천재 모임")
+            .category(checkCategory)
+            .state(checkState)
+            .images(images)
+            .ownerId("111")
+            .build();
+    }
+
+    public static final FindAllBooksVO secondDonationBook() {
+        List<String> checkCategory = new ArrayList<>();
+        checkCategory.add("SF");
+        checkCategory.add("만화");
+
+        List<String> checkState = new ArrayList<>();
+        checkState.add("깨끗");
+        checkState.add("거의 새책");
+
+        List<String> images = new ArrayList<>();
+        images.add("imgs1e51ca13-e32e-4db8-a308-bf4ccdd2c15f");
+        images.add("imgs0c1d16f2-a946-468f-8bcc-ffdc8f57b2fe");
+
+        return FindAllBooksVO.builder()
+            .id("113")
+            .postMessage("책 팜요 ~")
+            .title("읽으면 정말 좋은 책")
+            .author("천재 작가")
+            .publisher("천재 작가 모임")
+            .category(checkCategory)
+            .state(checkState)
+            .images(images)
+            .ownerId("111")
+            .build();
+    }
 }
